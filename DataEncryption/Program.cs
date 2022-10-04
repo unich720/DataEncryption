6381c31c6a05d837982461b8e589e680cf7a44a4
Dmitry Pomeshchikov <63234732+unich720@users.noreply.github.com>
2022-10-04 20:59:22 +0300

    tggf

--- DataEncryption/Program.cs
+++ DataEncryption/Program.cs
@@ -9,6 +9,79 @@ namespace DataEncryption
     {
         static async Task Main(string[] args)
         {
+            Console.WriteLine("1 Выбор алгоритма шифрования");
+            Console.WriteLine("2 Зашифровать");
+            Console.WriteLine("3 Расшифровать");
+
+            IAlgorithm algorithm = new Transposition();
+
+
+            while (true)
+            {
+                Console.WriteLine("Алгоритм: "+ algorithm.GetType().Name);
+                var sign = Convert.ToInt32(Console.ReadLine());
+                switch (sign)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("1 XOR");
+                            Console.WriteLine("2 Transposition");
+                            Console.WriteLine("3 Substitution");
+                            Console.WriteLine("4 OTP");
+                            var sign2 = Convert.ToInt32(Console.ReadLine());
+                            switch (sign2)
+                            {
+                                case 1:
+                                    {
+                                        algorithm = new XOR();
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        algorithm = new Transposition();
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        algorithm = new Substitution();
+                                        break;
+                                    }
+                                case 4:
+                                    {
+                                        algorithm = new OTP();
+                                        break;
+                                    }
+                                default:
+                                    break;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Зашивровал");
+                            var data = await FileSys.ReadFile("receipt.html");
+                            var keyLenght = 5;
+                            if (algorithm.GetType().Name == "Substitution")
+                            {
+                                keyLenght = 255;
+                            }
+                            await FileSys.GenerateKey(keyLenght, algorithm.GetType().Name);
+                            var t = MultiRound.EncryptAlg(algorithm, data, await FileSys.ReadKey(algorithm.GetType().Name));
+                            t.Wait();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Расшифровал");
+                            var data = await FileSys.ReadFile("receipt2.html");
+                            var ds = MultiRound.DecryptAlg(algorithm, data, await FileSys.ReadKey(algorithm.GetType().Name));
+                            ds.Wait();
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
             //Console.WriteLine("Hello World!");
 
             //var text = await FileSys.ReadFile("receipt.html");
